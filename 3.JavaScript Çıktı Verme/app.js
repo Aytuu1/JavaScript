1714ccb787c7ba14747b2ddd9bbe5d0f8aeaf5ad
Aytuu1 <Aytuuc1@gmail.com>
2024-10-11 18:15:12 +0300

    JavaScript Day 1

---
+++ 3.JavaScript Çıktı Verme/app.js
@@ -0,0 +1,19 @@
+// document write ile çıktı verme
+
+// document.write("Hello World<br>");
+// document.writeln("Aytuğ Atasever <br>");
+// document.write(5.5)
+
+//  console.log("Hello World");
+//  console.log("My name is JavaScript");
+//  console.clear()
+
+//  let a = 5;
+//  let b = 19;
+//  console.log("İki Sayının Toplamı:" + a + b);
+
+// Alert (Uyarı) popup ile çıktı verme işlemleri
+
+// console.log(window)
+
+// alert("Helloo");
