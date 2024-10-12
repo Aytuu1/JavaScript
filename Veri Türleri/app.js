b544149076a7684f672df2a419ea9c4188ceec70
Aytuu1 <Aytuuc1@gmail.com>
2024-10-12 17:57:12 +0300

    Veri Türleri

---
+++ Veri Türleri/app.js
@@ -0,0 +1,42 @@
+/*
+? Veri Tipleri
+
+1- String
+2- Number
+3- Boolean
+4- Null
+5- Undefined
+6- Object
+7- Function
+
+*/
+
+// String
+// let isim = "Enes";
+// console.log(isim);
+
+// Number
+// let sayi = 2;
+// console.log(sayi);
+
+// Boolean : True ve False' den oluşur.
+// let a = 5;
+// let b = 2;
+// let sonuc = a + b;
+// console.log(sonuc>10);
+
+// Null : Boş değer
+// let a = null;
+// console.log(a);
+
+// Undefined : Tanımlanmamış
+// let a;
+// console.log(a);
+
+// Object 
+// let insan={
+//     isim : "Ercan",
+//     soyIsim:"Şahin",
+//     yas:23
+// }
+// console.log(insan);
