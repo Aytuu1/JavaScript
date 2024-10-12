e2a420cb027acc004771ff7f62823b8976361a20
Aytuu1 <Aytuuc1@gmail.com>
2024-10-13 00:54:20 +0300

    Koşul Yapıları

---
+++ Koşul Yapıları/app.js
@@ -0,0 +1,52 @@
+/*
+  ? KOŞUL YAPILARI
+  */
+
+//   let not = 65;
+//   if (not > 50) 
+//     {
+//         console.log("Geçtiniz , notunuz :" + not);
+//     }
+//     else{
+//         console.log("Kaldınız, notunuz :" + not)
+//     }
+
+// let yas = Number(prompt("Yaşınız:"));
+// let para = Number(prompt("Bütceniz:")); 
+// if (yas >= 18 && para >= 3000) {
+//     alert("Ehliyet sınavına katılabilirsiniz.");
+// }
+// else{
+//     alert("Ehliyet Sınavına Katılamazsınız.");
+// }
+
+
+// --------- DERS ORTALAMASI ------
+// let vize1 = Number(prompt("1.Vize sınav notunu giriniz."));
+// let vize2 = Number(prompt("2.Vize sınav notunu giriniz."));
+// let final = Number(prompt("Final sınav notunu giriniz."))
+
+// let ortalama = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4) ;
+// if (ortalama >= 60 ) {
+//     alert("Geçtiniz.");
+// } else {
+//     alert("Kaldınız.");
+// }
+
+// ---- else if yapısı --
+
+// let secilenYol = prompt("Secmek istediginiz yolu giriniz.");
+// if (secilenYol == 1) 
+// {
+//     alert("Secilen yol " +secilenYol +".yoldur.")    
+// }
+// else if(secilenYol==2){
+//     alert("Secilen yol " +secilenYol +".yoldur.");
+// }
+// else if(secilenYol==3){
+//     alert("Secilen yol " +secilenYol +".yoldur.");
+// }
+// else{
+//     alert("Lütfen geçerli bir yol giriniz.");
+// }
+
