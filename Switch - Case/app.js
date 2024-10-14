754337ea0534d2e6984652d733570955af43e07e
Aytuu1 <Aytuuc1@gmail.com>
2024-10-15 01:07:13 +0300

    Switch - Case

---
+++ Switch - Case/app.js
@@ -0,0 +1,69 @@
+// Switch - Case
+// Haftanın günleri Uygulaması
+
+// let sayi =prompt("Lütfen 1 ile 5 arasında sayı giriniz.");
+// switch (sayi) {
+//     case "1":
+//         console.log("Girilen Sayı 1 dir");
+//         break;
+//     case "2":
+//         console.log("Girilen Sayı 2 dir");
+//         break;
+//     case "3":
+//         console.log("Girilen Sayı 3 dir");
+//         break;
+//     case "4":
+//         console.log("Girilen Sayı 4 dir");
+//         break;
+//     case "5":
+//         console.log("Girilen Sayı 5 dir");
+//         break;
+//     default:
+//         console.log("Lütfen Geçerli bir sayı giriniz.");
+//         break;
+// }
+// const yeniSatir = "\r\n";
+// let metin =
+//   "1-Pazartesi" +
+//   yeniSatir +
+//   "2-Salı" +
+//   yeniSatir +
+//   "3-Çarşamba" +
+//   yeniSatir +
+//   "4-Perşembe" +
+//   yeniSatir +
+//   "5-Cuma" +
+//   yeniSatir +
+//   "6-Cumartesi" +
+//   yeniSatir +
+//   "7-Pazar" +
+//   yeniSatir+"Lütfen bir değer seçiniz.";
+
+// let deger = prompt(metin);
+// switch (deger) {
+//   case "1":
+//     console.log("Pazartesi gününü seçtiniz.");
+//     break;
+//   case "2":
+//     console.log("Salı gününü seçtiniz.");
+//     break;
+//   case "3":
+//     console.log("Çarşamba gününü seçtiniz.");
+//     break;
+//   case "4":
+//     console.log("Perşembe gününü seçtiniz.");
+//     break;
+//   case "5":
+//     console.log("Cuma gününü seçtiniz.");
+//     break;
+//   case "6":
+//     console.log("Cumartesi gününü seçtiniz.");
+//     break;
+//   case "7":
+//     console.log("Pazar gününü seçtiniz.");
+//     break;
+
+//   default:
+//     console.log("Lütfen Geçerli bir gün giriniz.");
+//     break;
+// }
