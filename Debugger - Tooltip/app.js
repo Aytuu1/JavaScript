ba12aa3b77f2ce4c6cb6ab27529bb64af1fbb520
Aytuu1 <Aytuuc1@gmail.com>
2024-10-17 18:00:49 +0300

    Debugger-ToolTip

---
+++ Debugger - Tooltip/app.js
@@ -0,0 +1,13 @@
+// Breakpoint ve Tooltip Nedir ?
+// ToolTip: Yazılımcılara bilgi veren mesaj kutucukları
+
+// BreakPoint: Yazılan kodu adım adım çalıştırır ve oluşan hatayı bulmaya yarar.
+
+let a = 10;
+debugger;
+let b = 15;
+let c = a + b;
+let d = a+b+c;
+let e = a+c+b;
+let sonuc = a+b+c+d+e;
+console.log("Sonuç:",sonuc);
