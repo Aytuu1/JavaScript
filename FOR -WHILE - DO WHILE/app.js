edbfbcffa408621f1b309bfb8e209b7e45684c2e
Aytuu1 <Aytuuc1@gmail.com>
2024-10-17 18:39:14 +0300

    DONGULER (LOOPS)

---
+++ FOR -WHILE - DO WHILE/app.js
@@ -0,0 +1,90 @@
+// FOR DÖNGÜSÜ 
+// for (let i =10; i<=10; i++){
+//     console.log(i);
+// }
+
+// 1 den 10 a kadar çiftleri yazdıralım
+// for(let i=0; i<=10; i=i+2 ){
+//     console.log(i);
+// }
+
+// 1'den 10'a kadar tekleri yazdıralım
+// for(let i=1; i<=10; i=i+2 ){
+    //     console.log(i);
+    // }
+
+// for (let i =1; i<=10; i++){
+//     if(i%2==1){
+//         console.log("Merhaba");
+//     }
+//     else{
+//         console.log("Naber");
+//     }
+// }
+
+// 50'den 1 e kadar gidelim ve toplamlarını bulalım
+// toplam=0;
+// for (let i=50; i>=1; i--){
+//     toplam=toplam+i;
+//     console.log(i);
+// }
+// console.log("Toplam:",toplam);
+
+// ? WHILE DONGUSU
+// 1'den 10'a kadar yazdıralım
+// let sayac = 1;
+// while(sayac<=10){
+//     console.log(sayac);
+//     sayac++;
+// }
+
+// 1'den 10'a kadar çiftleri ve tekleri yazdıralım.
+// let sayac=0;
+// while(sayac<=10){
+//     if(sayac%2==1){
+//         console.log(sayac);
+//     }
+//     sayac++;
+// }
+// let sayac=1;
+// while(sayac<=10){
+//     if(sayac%2==0){
+//         console.log(sayac);
+//     }
+//     sayac++;
+// }
+
+// let sayac=1;
+// while (true) {
+//     console.log(sayac);
+//     if (sayac==7) {
+//         break;
+//     }
+//     sayac++;
+// }
+
+// ? DO WHILE DONGUSU
+
+//1'den 10'a kadar yazdıralım
+// let sayac=1;
+// do {
+//     console.log(sayac);
+//     sayac++;
+// } while (sayac<=10);
+
+// let yas = 23;
+// do {
+//     console.log("Ehliyeti Alabilirsiniz.");
+// } while (yas>=25);
+
+// 1'den 20 ' ye kadar olan tek sayıların toplamını bulalım.
+
+// let sayac =1;
+// let toplam=0;
+// do {
+//     if (sayac%2==1) {
+//         toplam+=sayac;
+//     }
+//     sayac++;
+// } while (sayac<=20);
+// console.log("Toplam:",toplam);
