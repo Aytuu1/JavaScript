64feb8091b1956e61add2a27a7b0ca3f4077ea59
Aytuu1 <Aytuuc1@gmail.com>
2024-10-17 17:40:19 +0300

    Tur Donusumleri (Data Types)

---
+++ TURDONUSUM/app.js
@@ -0,0 +1,29 @@
+/*
+  ? TÜR DÖNÜŞÜMLERİ 
+  
+  strings, numbers, booleans,undefined and null.
+  object ,function
+ */
+
+  // string veri tipinden number veri tipine dönüştürme
+  // let a = 5;
+  // let b =Number("10");
+
+  //ParseInt();
+// let a = 8;
+// let b =parseInt("10");
+
+//Number Tipinden String Veri Tipine Dönüştürmek 
+
+// let x =String(55);
+// console.log(typeof x);
+
+// let x = (55).toString();
+// console.log(typeof x);
+
+// let sonuc = String(true);
+// console.log(typeof sonuc);
+
+// let rakamlar = String([1,2,3,4]);
+// console.log(typeof rakamlar);
+
