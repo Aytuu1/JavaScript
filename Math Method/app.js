0d9651434c845709c953eae1f6d43910660cfbcf
Aytuu1 <Aytuuc1@gmail.com>
2024-11-13 21:20:12 +0300

    Math Method

---
+++ Math Method/app.js
@@ -0,0 +1,49 @@
+//? Math Sınıfı Methodları
+/*      
+1 -> Floor 
+2 -> Ceil
+3 -> Round
+
+4 -> Max
+5 -> Min
+
+6 -> Random
+7 -> Abs
+8 -> Sqrt
+9 -> Pow
+*/
+
+//! Floor kullanımı -> Noktadan sonra ki verilen sayısal değeri görmez ve Tabana yuvarlar.
+// let a = 3.15;
+// console.log(Math.floor(a))
+
+//! Ceil Kullanımı -> Noktodan sonra ki verilen sayısal değeri görmez ve Tavana Yuvarlar.
+// let a = 3.0;
+// console.log(Math.ceil(a));
+
+//! Round() -> Noktodan sonra ki değeri en yakın olan sayıya yuvarlar.
+// let a = 3.6;
+// console.log(Math.round(a));
+
+//! Max() -> Verilen dizi içinde ki sayılardan en büyük olanını döner.
+// let Number = [1,2,3,4,5,50];
+// console.log(Math.max(...Number));
+
+//! Min() -> Verilen dizi içinde ki sayılardan en küçük olanını döner.
+// let number = [1,2,3,4,5,45];
+// console.log(Math.min(...number));
+
+//! Abs() -> Mutlak değer almak için kullanılır.
+// let b = -12;
+// console.log(Math.abs(b));
+
+//! Sqrt() -> Karekök almak için kullanılır.
+// let b = 25;
+// console.log(Math.sqrt(b));
+
+//! Pow() -> Üssünü almak için kullanılır.
+// let b =5;
+// console.log(Math.pow(b,2))
+
+//! Random() -> Rastgele değerler üretmek için kullanılır.
+// console.log(Math.round(Math.random()*100));
