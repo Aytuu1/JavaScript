6dbe7e99886f03040513f1a36d0425daa1cef419
Aytuu1 <Aytuuc1@gmail.com>
2024-11-15 14:51:42 +0300

    Date Methods

---
+++ Date Methods/app.js
@@ -0,0 +1,39 @@
+//? Date Kullanımı
+
+ let date = new Date();
+// console.log(date);
+
+//!console.log(date.getFullYear());
+ // İçinde bulunulan yılı alır.
+
+//!console.log(date.getDate());
+ // İçinde bulunulan ayın gününü alır.
+
+//!console.log(date.getDay());
+// Haftanın kaçıncı gününde olduğunu alır.
+
+//!console.log(date.getHours());
+ // İçinde bulunulan saati alır.
+
+//!console.log(date.getMinutes());
+// İçinde bulunulan dakikayı alır.
+
+//!console.log(date.getMonth());
+// İçinde bulunulan ay'ı alır aylar 0'dan başlar.;
+
+//!console.log(date.toLocaleDateString()); 
+// Gün / Ay /Yıl olarak çıktı verir.
+
+//! console.log(date.toLocaleTimeString());
+// Saat / DAKİKA / SANİYE olarak çıktı verir.
+
+//? Set Methodları
+console.log(date); // Değiştirilmeden önceki
+
+date.
+date.setMonth(11);// ay değiştirildikten sonra
+date.setHours(15); // Saat eklendikten sonra
+date.setDate(24); // 24 gün eklendikten sonraki
+console.log(date);
+
+
