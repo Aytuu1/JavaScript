c06d765ec10cd338de83670b79f2c89018980c5a
Aytuu1 <Aytuuc1@gmail.com>
2024-11-15 17:31:27 +0300

    Document Object

---
+++ Document Object/app.js
@@ -0,0 +1,27 @@
+//? Document Object
+
+let value;
+
+//value = document.characterSet; //! Kullanılan karakter seti : UTF8'i verir.
+
+// value = document.title; //! Sayfada ki başlığı verir.
+
+// value = document.links.item(3); //! Sayfada bulunan linklerden 3'üncüsünü getirir.
+
+// value = document.links.item(3).id; //! Sayfada bulunan linklerden 3'üncüsünün id'sini getirir.
+
+// value = document.links.item(3).getAttribute("id"); //! Sayfada bulunan linklerden 3'üncüsünün id'sini getirir.
+
+// value = document.links.item(3).getAttribute("class"); //! Sayfada bulunan linklerden 3'üncüsünün class'larını getirir.
+
+
+// value = document.links.item(3).classList[3]; //! Sayfada bulunan linklerden 3'üncüsünün class'larından 3.sünü getirir.
+
+// value = document.forms; //! Sayfada ki formların tamamını getirir.
+
+//value = document.forms.item(1).getAttribute("id"); //! Sayfada ki form etiketlerinden index değeri 1 olanın id değerini getirir.
+
+//value = document.forms.item(1).children[0].classList[1]; //! Sayfada ki form etiketlerinden index değeri 1 olanı getirir ve içinde bulunan childlardan 0 indexli olanın classlarından index değeri 1 olanı ekrana basar.
+
+
+console.log(value);
