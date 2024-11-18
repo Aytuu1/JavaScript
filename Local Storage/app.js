eb6666a53423125b58bfc9b0ea434eea1895548b
Aytuu1 <Aytuuc1@gmail.com>
2024-11-18 22:39:34 +0300

    Local Storage

---
+++ Local Storage/app.js
@@ -0,0 +1,30 @@
+//? ------------ Local Storage Kullanımı -----------
+
+ //?  ------------ Değer Ekleme ---------
+// localStorage.setItem("motion","Push up");
+// localStorage.setItem("motion2","Barfix");
+// localStorage.setItem("motion3","Burpee");
+// localStorage.setItem("motion4","Squat");
+
+//? ------------ Değer Alma ----------
+// let value = localStorage.getItem("motion");
+// console.log(value);
+
+//? ------ Değer Silmek ---------
+
+// localStorage.removeItem("motion4");
+
+//? -------- Tümünü Temizle ------
+
+// localStorage.clear();
+
+
+//? ----------- localStorage Array'a çevirme ------
+
+// let motions = ["Barfix","Squat","Burpee","Push up"];
+
+// localStorage.setItem("motions",JSON.stringify(motions));
+
+// let array = JSON.parse(localStorage.getItem("motions"));
+
+// console.log(array);
