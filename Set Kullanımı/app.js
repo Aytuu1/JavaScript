1ddeb2c9018c88bc6babbe60a473f75f617932f1
Aytuu1 <Aytuuc1@gmail.com>
2024-11-23 01:19:07 +0300

    Template Literals

---
+++ Set Kullanımı/app.js
@@ -0,0 +1,16 @@
+// //? Set Kullanımı (Değerler sadece 1 kere tutulur.);
+
+// const set = new Set();
+
+// set.add("true");
+// set.add(3.14);
+// set.add("Enes");
+// set.add(7);
+// set.add({userName:"Enes",password:"1"});
+// set.add([1,2,3,4]);
+
+// set.delete("Enes");
+// console.log(set.has("Enes"));
+// console.log(set.has(7));
+
+// console.log(set.size);
---
+++ Template Literals/app.js
@@ -0,0 +1,13 @@
+//? Template Literals (ALT GR + İKİ KEZ NOKTALI VİRGÜL) => ``
+//! Old way (Eski yol)
+// function write(firstName,lastName){
+//     console.log("İsim : "+firstName + " Soyisim : "+ lastName);
+// }
+// write("Burak","Dağ");
+
+//! New Way (Yeni yol)
+
+// function write(firstName,lastName){
+//     console.log(`isim:${firstName} Soyisim:${lastName}`);
+// }
+// write("Burak","Dağ");
