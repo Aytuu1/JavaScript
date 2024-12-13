b7317eff51d3dfe716e025069d1721ed9ecf4404
Aytuu1 <Aytuuc1@gmail.com>
2024-12-13 16:13:40 +0300

    searchImagesProject

---
+++ searchImages_Project/app.js
@@ -0,0 +1,54 @@
+const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imagelistWrapper = document.querySelector(".imagelist-wrapper");
+
+runEventListener();
+
+function runEventListener(){
+    form.addEventListener("submit",Search);
+    clearButton.addEventListener("click",clear);
+}
+function clear(){
+    searchInput.value="";
+    Array.from(imagelistWrapper.children).forEach((child)=>{
+        child.remove();
+    });
+    // imagelistWrapper.innerHTML="";
+}
+
+function Search(e){
+    const value = searchInput.value.trim();
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`,{
+        method  : "GET",
+        headers :{
+            Authorization :"Client-ID SKEvg5Er1JS2Jsceq75rPgi3fBEfdfic94PLiLfPgNw"
+        }
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+        Array.from(data.results).forEach((image)=>{
+            // console.log(image.urls.small);
+            addImageToUI(image.urls.small);
+        })
+    })
+    .catch((err)=>console.log(err))
+    e.preventDefault();
+}
+
+
+function addImageToUI(url){
+    const div = document.createElement("div");
+    div.className="card";
+
+    const img = document.createElement("img");
+    img.setAttribute("src", url);
+    img.height='400';
+    img.width='400';
+
+    div.append(img);
+    imagelistWrapper.append(div);
+}
